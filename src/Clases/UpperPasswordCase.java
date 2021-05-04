5fa38f0425a4fd6136fbe489a9bd952ee05cf018
carra <carraborrosa@gmail.com>
2021-05-04 22:51:00 +0200

    yy

--- src/Clases/UpperPasswordCase.java
+++ src/Clases/UpperPasswordCase.java
@@ -26,35 +26,39 @@ public class UpperPasswordCase {
         this.locker = locker;
     }
 
-
+    /**
+     *
+     * @param attempt
+     * @param path
+     * @return
+     * @throws IOException
+     */
     public File openFile(String attempt, String path) throws IOException {
         File file = new File (path);
-            if (file.exists ( )) {
-                BufferedReader br = new BufferedReader (new FileReader (file));
-                BufferedWriter bw = new BufferedWriter (new FileWriter (this.fileToWrite));
-                String line = br.readLine ();
-                while (line != null) {
-                    String coso = "";
-                    coso += line.toCharArray ( )[0];
-                    coso = coso.toUpperCase ( );
-                    coso += line.toCharArray ( )[1];
-                    for (int i = 2; i < line.length ( ); i++) {
-                        if ((line.toCharArray ( )[i - 1] == ' ' && line.toCharArray ( )[i - 2] == '.') || line.toCharArray ( )[i - 1] == '.' ) {
-                            String support = "";
-                            support += line.charAt (i);
-                            coso += support.toUpperCase ( );
-                        } else {
-                            coso += line.toCharArray ( )[i];
-
-                        }
+        if (file.exists ( )) {
+            BufferedReader br = new BufferedReader (new FileReader (file));
+            BufferedWriter bw = new BufferedWriter (new FileWriter (this.fileToWrite));
+            String line = br.readLine ( );
+            while (line != null) {
+                String coso = "";
+                coso += line.toCharArray ( )[0];
+                coso = coso.toUpperCase ( );
+                coso += line.toCharArray ( )[1];
+                for (int i = 2; i < line.length ( ); i++) {
+                    if ((line.toCharArray ( )[i - 1] == ' ' && line.toCharArray ( )[i - 2] == '.') || line.toCharArray ( )[i - 1] == '.') {
+                        String support = "";
+                        support += line.charAt (i);
+                        coso += support.toUpperCase ( );
+                    } else {
+                        coso += line.toCharArray ( )[i];
                     }
-                    bw.write (coso);
-                    line = br.readLine ( );
                 }
-                bw.close ( );
-                br.close ( );
+                bw.write (coso);
+                line = br.readLine ( );
             }
-
+            bw.close ( );
+            br.close ( );
+        }
         return file;
     }
 }
